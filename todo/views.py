320ed7fb62953de1a9bf1a351e8824b81ae635e5
Olha <olgazinevych@gmail.com>
2024-07-18 16:53:36 +0300

    feat: create custom task form to allow marking tags as checkboxes when creating/updating tasks

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views import generic
 
+from .forms import TaskForm
 from .models import Task
 
 
@@ -10,12 +11,13 @@ class TaskListView(generic.ListView):
 
 
 class TaskCreateView(generic.CreateView):
-    model = Task
-    fields = "__all__"
+    form_class = TaskForm
+    template_name = "todo/task_form.html"
     success_url = reverse_lazy("todo:task-list")
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
-    fields = "__all__"
+    form_class = TaskForm
+    template_name = "todo/task_form.html"
     success_url = reverse_lazy("todo:task-list")
