529f9cd60a3006560a47849995e2c5db72a3435e
Olha <olgazinevych@gmail.com>
2024-07-18 16:29:01 +0300

    feat: add task create view and a path to it

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import TaskListView
+from .views import TaskListView, TaskCreateView
 
 app_name = "todo"
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="task-list")
+    path("", TaskListView.as_view(), name="task-list"),
+    path("create/", TaskCreateView.as_view(), name="task-create"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,4 @@
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .models import Task
@@ -6,3 +7,9 @@ from .models import Task
 class TaskListView(generic.ListView):
     model = Task
     queryset = Task.objects.prefetch_related("tags")
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task-list")
