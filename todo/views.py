5a376814a00ce51afdfbc2d1fba33c5ff34ffc05
Olha <olgazinevych@gmail.com>
2024-07-18 16:07:53 +0300

    feat: add task list view to home page and core task_list template

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Task
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.prefetch_related("tags")
