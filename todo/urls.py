6f3aba920013c944cccc6df662f88b8925f5687d
Olha <olgazinevych@gmail.com>
2024-07-18 16:46:59 +0300

    feat: add path to urls, update view and button to update a task to task list

--- todo/urls.py
+++ todo/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import TaskListView, TaskCreateView
+from .views import TaskListView, TaskCreateView, TaskUpdateView
 
 app_name = "todo"
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("create/", TaskCreateView.as_view(), name="task-create"),
+    path("<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -13,3 +13,9 @@ class TaskCreateView(generic.CreateView):
     model = Task
     fields = "__all__"
     success_url = reverse_lazy("todo:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("todo:task-list")
